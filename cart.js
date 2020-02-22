069ebe9265464def7a7c57411b8994d5bba86d74
Klaudia Tomanova <klaudiatomanova@Klaudias-MacBook-Air.local>
2020-02-22 13:28:10 +0000

    made some changes to cart.js

--- cart.js
+++ cart.js
@@ -1,39 +1,24 @@
-let cart = [];
+const shoppingCart = [];
+const stock = [];
 
-let Item = function(name, price, count) {
+const item = function(id, name, price, qty) {
+  this.id = id;
   this.name = name;
   this.price = price;
-  this.count = count;
+  this.qty = qty;
 };
 
-let obj = {};
+const stockItem = function(id, name, price, imagefile) {
+  this.id = id;
+  (this.name = name), (this.price = price);
+  this.imagefile = imagefile;
+};
 
-obj.addItemToCart = function(name, price, count) {
-  for (let item in cart) {
-    if (cart[item].name === name) {
-      cart[item].count++;
-      return;
-    }
-  }
-  let item = new Item(name, price, count);
-  cart.push(item);
-};
-obj.setCountForItem = function(name, count) {
-  for (let i in cart) {
-    if (cart[i].name === name) {
-      cart[i].count = count;
-      break;
-    }
-  }
-};
-obj.removeItemFromCart = function(name) {
-  for (let item in cart) {
-    if (cart[item].name === name) {
-      cart[item].count--;
-      if (cart[item].count === 0) {
-        cart.splice(item, 1);
-      }
-      break;
-    }
-  }
-};
+function init() {
+  loadStock();
+}
+
+function loadStock() {}
+
+//helper function - function can call another function
+function findItemInStock() {}
